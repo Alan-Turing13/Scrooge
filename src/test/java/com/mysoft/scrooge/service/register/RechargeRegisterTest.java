098f790a325579a285468f7e385843cbfb18e5f7
hobovsky <hobson@wp.pl>
2021-07-08 00:26:34 +0200

    Initial test

---
+++ src/test/java/com/mysoft/scrooge/service/register/RechargeRegisterTest.java
@@ -0,0 +1,14 @@
+package com.mysoft.scrooge.service.register;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class RechargeRegisterTest extends RegisterTestBase {
+
+    @Test
+    public void givenAnEmptyRegister_WhenRechargedWith13_ThenBalanceOf13IsPersisted() {
+        fail("Test not implemented");
+    }
+
+}
