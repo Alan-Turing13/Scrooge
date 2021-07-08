9691385b9f31b2ec0986f04de2ce7d1a92ef60d8
hobovsky <hobson@wp.pl>
2021-07-08 02:22:18 +0200

    Create initial demo data

--- src/main/java/com/mysoft/scrooge/model/Register.java
+++ src/main/java/com/mysoft/scrooge/model/Register.java
@@ -10,7 +10,7 @@ import java.math.BigDecimal;
 public class Register {
 
     @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
     private String displayName;
     private BigDecimal balance = BigDecimal.ZERO;
