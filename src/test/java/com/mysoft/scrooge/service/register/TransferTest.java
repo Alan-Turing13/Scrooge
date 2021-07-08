f884e19ad9ff6b3f9531eda36ee2760481fd101b
hobovsky <hobson@wp.pl>
2021-07-08 03:01:03 +0200

    Initial test

---
+++ src/test/java/com/mysoft/scrooge/service/register/TransferTest.java
@@ -0,0 +1,17 @@
+package com.mysoft.scrooge.service.register;
+
+import com.mysoft.scrooge.model.Register;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class TransferTest extends RegisterTestBase {
+
+    @Test
+    public void givenTwoRegisters_WhenTransferringAValidAmount_BalanceOfBothIsUpdated() {
+        fail("Test not implemented");
+    }
+
+}
